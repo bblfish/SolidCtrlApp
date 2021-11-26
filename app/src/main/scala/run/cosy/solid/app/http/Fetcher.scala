5633ca3ad8b2afb2124d5459d816e686d11a5594
Henry Story <henry.story@bblfish.net>
2021-11-26 07:37:44 +0100

    reformat

--- app/src/main/scala/run/cosy/solid/app/http/Fetcher.scala
+++ app/src/main/scala/run/cosy/solid/app/http/Fetcher.scala
@@ -2,45 +2,46 @@ package run.cosy.solid.app.http
 
 import cats.effect.*
 import cats.effect.unsafe.implicits.*
-import org.http4s.client
-import org.http4s.dom.*
-import org.http4s.{Request,QValue}
-import org.http4s.implicits.*
-import org.http4s.Method.GET
+import fs2.{INothing, text}
 import org.http4s.MediaType.text.turtle
-import cats.effect.IO
-import scala.scalajs.js.annotation.{JSExport,JSExportTopLevel}
-import fs2.text
+import org.http4s.Method.GET
+import org.http4s.dom.*
+import org.http4s.implicits.*
+import org.http4s.{QValue, Request, client}
 import run.cosy.app.io.n3.N3Parser
-import fs2.INothing
+
 import java.nio.charset.Charset
+import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
 @JSExportTopLevel("Fetcher")
 object Fetcher {
+
 	import org.scalajs.dom
 	import org.scalajs.dom.document
 
 	val clnt: client.Client[IO] = FetchClientBuilder[IO].create
 	// val rdfHeaders = org.http4s.Headers
-	import org.http4s.headers.*
+
 	import org.http4s.client.dsl.io.given
+	import org.http4s.headers.*
+
 	val req: Request[IO] = GET(
 		uri"https://bblfish.net/people/henry/card",
 		Accept(turtle.withQValue(QValue.One))
 	)
-	// // clnt.run(req)
+
 	lazy val answer: IO[String] = clnt.expect[String](req)
-	
+
 	@JSExport
-	def addClickedMessage(): Unit = 
+	def addClickedMessage(): Unit =
 		appendPar(document.body, "You clicked the button!")
 		onClick()
-	
+
 	def appendPar(targetNode: dom.Node, text: String): Unit =
 		val parNode = document.createElement("p")
 		parNode.textContent = text
 		targetNode.appendChild(parNode)
-	
+
 	def main(args: Array[String]): Unit =
 		document.addEventListener("DOMContentLoaded", { (e: dom.Event) =>
 			println(e)
@@ -61,18 +62,15 @@ object Fetcher {
 	def onClick(): Unit =
 		val utfStr: fs2.Stream[cats.effect.IO, String] = clnt.stream(req).flatMap(_.body)
 			.through(text.utf8.decode)
-	
+
 		val ios: fs2.Stream[cats.effect.IO, INothing] = utfStr.through(N3Parser.parse)
-			.foreach { triple => 
+			.foreach { triple =>
 				IO(appendPar(document.body, triple.toString))
 			}
 
-		ios.compile.lastOrError.unsafeRunAsync{
-			case Left(err)     => appendPar(document.body, err.toString)
+		ios.compile.lastOrError.unsafeRunAsync {
+			case Left(err) => appendPar(document.body, err.toString)
 			case Right(answer) => appendPar(document.body, "good answer")
 		}
-
-//	cl.expect
-//
 
 }
--- app/src/test/scala/run/cosy/solid/app/http/FetcherMunitTests.scala
+++ app/src/test/scala/run/cosy/solid/app/http/FetcherMunitTests.scala
@@ -1,12 +1,13 @@
 package run.cosy.solid.app.http
 
-import scala.scalajs.js
+import fs2.{Chunk, Pure, Stream}
 import org.scalajs.dom
 import org.scalajs.dom.document
 import org.scalajs.dom.ext.*
-import fs2.{Chunk, Pure, Stream}
 import run.cosy.app.io.n3.N3Parser
 import run.cosy.rdfjs.model.Quad
+
+import scala.scalajs.js
 
 
 class FetcherMunitTests extends munit.FunSuite {
@@ -17,7 +18,9 @@ class FetcherMunitTests extends munit.FunSuite {
 	}
 
 	def bbl(tag: String): String = "https://bblfish.net/"
+
 	def w3c(tag: String): String = "https://w3.org/"
+
 	def foaf(tag: String): String = "https://xmlns.com/foaf/0.1/"
 
 	val ntriples1: String =
@@ -26,8 +29,8 @@ class FetcherMunitTests extends munit.FunSuite {
 			|""".stripMargin
 	val ntriples2: String =
 		s"""<${w3c("timbl/card#i")}> <${foaf("knows")}> <${bbl("#me")}> .
-			 |<${bbl("timbl/card#i")}> <${foaf("name")}> "Tim Berners-Lee"@en .
-			 |""".stripMargin
+			|<${bbl("timbl/card#i")}> <${foaf("name")}> "Tim Berners-Lee"@en .
+			|""".stripMargin
 
 	test("Simple NTriples line Chunk Test") {
 		val bigNTriples = ntriples1 + ntriples2
@@ -40,22 +43,22 @@ class FetcherMunitTests extends munit.FunSuite {
 		assertEquals(stats.size, 4)
 
 		//parse one chunk of two strings
-		val chunkedStatements: Stream[Pure, Quad] = N3Parser.parse(Stream(ntriples1,ntriples2))
+		val chunkedStatements: Stream[Pure, Quad] = N3Parser.parse(Stream(ntriples1, ntriples2))
 		val chkdSt = chunkedStatements.toList.toSet
-		assertEquals(chkdSt.size,4)
-		assertEquals(stats,chkdSt)
+		assertEquals(chkdSt.size, 4)
+		assertEquals(stats, chkdSt)
 
 		//parse many chunks of many strings
 		//split the doc into strings of max 11 chars.
-		val chunkedStream: Stream[Pure, String] = Stream(bigNTriples.sliding(11,11).toList*)
+		val chunkedStream: Stream[Pure, String] = Stream(bigNTriples.sliding(11, 11).toList *)
 		//partition those strings into chunks of three
 		val streamOfChunkStr: Stream[Pure, String] = chunkedStream.chunkN(3).flatMap(cs => Stream.chunk(cs))
 		assertEquals(streamOfChunkStr.toList.size, 27, "Total number of strings")
 		assertEquals(streamOfChunkStr.chunks.toList.size, 9, "Total number of chunks")
 
-		val buStream: Stream[Pure,Quad] = N3Parser.parse(streamOfChunkStr)
+		val buStream: Stream[Pure, Quad] = N3Parser.parse(streamOfChunkStr)
 		val bs = buStream.toList.toSet
-		assertEquals(bs.size,4)
-		assertEquals(bs,stats)
+		assertEquals(bs.size, 4)
+		assertEquals(bs, stats)
 	}
 }
