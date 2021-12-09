b6e8fe85241ab97d6202699dc95de816341051e2
Henry Story <henry.story@bblfish.net>
2021-12-09 17:01:28 +0100

    improved AuthN tests

--- authn/src/test/scala/run/cosy/solid/app/auth/AuthNTest.scala
+++ authn/src/test/scala/run/cosy/solid/app/auth/AuthNTest.scala
@@ -3,18 +3,18 @@ package run.cosy.solid.app.auth
 import cats.effect.*
 import cats.effect.std.Semaphore
 import cats.syntax.all.*
+import org.http4s.Uri.Host
 import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
+import org.http4s.client.middleware.ResponseLogger
 import org.http4s.dsl.io.{->, Ok, Unauthorized, *}
 import org.http4s.headers.*
-import org.http4s.server.AuthMiddleware
 import org.http4s.server.middleware.authentication.BasicAuth
+import org.http4s.server.{AuthMiddleware, Router}
 import org.http4s.syntax.all.*
-import org.http4s.{AuthedRoutes, BasicCredentials, Challenge, Headers, Request, Response, Status, Uri}
+import org.http4s.{AuthedRoutes, BasicCredentials, Challenge, Headers, HttpRoutes, Request, Response, Status, Uri}
 import org.typelevel.ci.*
 import run.cosy.app.auth.AuthN
-import org.http4s.Uri.Host
-import org.http4s.client.middleware.ResponseLogger
 
 import java.util.concurrent.atomic.*
 
@@ -26,7 +26,11 @@ class AuthNTest extends munit.CatsEffectSuite {
 	val username = "Test User"
 	val password = "Test Password"
 
-	val service: AuthedRoutes[String, IO] = AuthedRoutes.of[String, IO] {
+	def publicRoutes: HttpRoutes[IO] = HttpRoutes.of[IO] {
+		case GET -> Root => Ok("Hello World")
+	}
+
+	val authedRoutes: AuthedRoutes[String, IO] = AuthedRoutes.of[String, IO] {
 		case GET -> Root as user => Ok(user)
 		case req as _ => Response.notFoundFor(req)
 	}
@@ -39,61 +43,114 @@ class AuthNTest extends munit.CatsEffectSuite {
 
 	val basicAuthMiddleware: AuthMiddleware[IO, String] = BasicAuth(realm, validatePassword)
 
-   //
+
+	//
 	// test server
 	//
 	{
-		val basicAuthedService = basicAuthMiddleware(service)
+		val basicAuthedService = basicAuthMiddleware(authedRoutes)
+
+		def routes: HttpRoutes[IO] = Router[IO](
+			"/pub" -> publicRoutes,
+			"/auth" -> basicAuthedService
+		)
+
+		test("public Route needs no authentication") {
+			routes(Request[IO](uri = uri"/pub/")).map { (res: Response[IO]) =>
+				assertEquals(res.status, Status.Ok)
+			}
+		}
 
 		test("BasicAuthentication should respond to a request with unknown username with 401") {
 			val req = Request[IO](
-				uri = uri"/",
+				uri = uri"/auth",
 				headers = Headers(Authorization(BasicCredentials("Wrong User", password)))
 			)
-			basicAuthedService.orNotFound(req).map { (res: Response[IO]) =>
-				assertEquals(res.status, Unauthorized)
-				assertEquals(
-					res.headers.get[`WWW-Authenticate`].map(_.value),
-					Some(Challenge("Basic", realm, Map("charset" -> "UTF-8")).toString)
-				)
+			routes(req).foldF(IO(fail("no route"))) { (res: Response[IO]) =>
+				IO(assertEquals(res.status, Status.Unauthorized)) >>
+					IO(assertEquals(
+						res.headers.get[`WWW-Authenticate`].map(_.value),
+						Some(Challenge("Basic", realm, Map("charset" -> "UTF-8")).toString)
+					))
+			}
+		}
+
+		test("BasicAuthentication should fail to respond to a request for non existent resource") {
+			val req = Request[IO](
+				uri = uri"/doesNotExist",
+				headers = Headers(Authorization(BasicCredentials("Wrong User", password)))
+			)
+			routes(req).foldF(IO(assert(true, "route does not exist"))) { (res: Response[IO]) =>
+				IO(fail("route does not exist"))
 			}
 		}
 
 		test("BasicAuthentication should respond to a request with correct credentials") {
 			val req = Request[IO](
-				uri = uri"/",
+				uri = uri"/auth",
 				headers = Headers(Authorization(BasicCredentials(username, password)))
 			)
-			basicAuthedService
-				.orNotFound(req)
-				.map(_.status)
-				.assertEquals(Ok)
+			routes(req).foldF(IO(fail("no route"))) { (res: Response[IO]) =>
+				IO(assertEquals(res.status, Status.Ok)) >>
+					res.as[String].map(s => assertEquals(s, username))
+			}
 		}
 
+		test("BasicAuthentication responds to authenticated non-existent resource with 404") {
+			val req = Request[IO](
+				uri = uri"/auth/nonExistent",
+				headers = Headers(Authorization(BasicCredentials(username, password)))
+			)
+			routes(req).foldF(IO(fail("no route"))) { (res: Response[IO]) =>
+				IO(assertEquals(res.status, Status.NotFound))
+			}
+		}
+
+
 		// test with client now
-		val defaultClient: Client[IO] = Client.fromHttpApp(basicAuthedService.orNotFound)
-//		val logedClient: Client[IO] = ResponseLogger[IO](true, true, logAction = Some(s => IO(println(s))))(defaultClient)
+		val defaultClient: Client[IO] = Client.fromHttpApp(routes.orNotFound)
+		//		val logedClient: Client[IO] = ResponseLogger[IO](true, true, logAction = Some(s => IO(println(s))))(defaultClient)
 		val client: Client[IO] = AuthN[IO](AuthN.basicWallet(
-			Map(Uri.RegName("localhost") -> new AuthN.BasicId(username,password))
+			Map(Uri.RegName("localhost") -> new AuthN.BasicId(username, password))
 		))(defaultClient)
 
-		test("basic authentication with client") {
-			client.get(uri"http://localhost/"){ (res: Response[IO]) =>
+		val clientBad: Client[IO] = AuthN[IO](AuthN.basicWallet(
+			Map(Uri.RegName("localhost") -> new AuthN.BasicId(username, password + "bad"))
+		))(defaultClient)
+
+		test("Wallet Based Auth") {
+			client.get(uri"http://localhost/auth") { (res: Response[IO]) =>
 				IO(assertEquals(res.status, Status.Ok)) >>
 					res.as[String].map(s => assertEquals(s, username))
 
 			}
 		}
 
-		test("no authentication on non-existent resource") {
-			client.get(uri"http://localhost/NotFound"){ (res: Response[IO]) =>
-				IO(assertEquals(res.status, Status.NotFound))
+		test("Wallet Based Auth on Non Existent resource") {
+			client.get(uri"http://localhost/auth") { (res: Response[IO]) =>
+				IO(assertEquals(res.status, Status.Ok)) >>
+					res.as[String].map(s => assertEquals(s, username))
+
 			}
 		}
 
+
+		test("Wallet Based Auth with bad password fails on protected resources") {
+			clientBad.get(uri"http://localhost/auth") { (res: Response[IO]) =>
+				IO(assertEquals(res.status, Status.Unauthorized))
+			}
+			clientBad.get(uri"http://localhost/auth/NonExistent") { (res: Response[IO]) =>
+				IO(assertEquals(res.status, Status.Unauthorized))
+			}
+		}
+
+		test("Wallet Based Auth with bad password succeeds on public resources") {
+			clientBad.get(uri"http://localhost/pub") { (res: Response[IO]) =>
+				IO(assertEquals(res.status, Status.Ok)) >>
+					res.as[String].map(s => assertEquals(s, "Hello World"))
+			}
+		}
 	}
-
-
 
 
 }
