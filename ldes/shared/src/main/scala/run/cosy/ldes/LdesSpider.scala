e0f718cbedf0ecc43985950cc68108560ea3632d
Henry Story <henry.story@bblfish.net>
2023-03-16 16:56:48 +0100

    seperate the crawling and the extracting

--- ldes/shared/src/main/scala/run/cosy/ldes/LdesSpider.scala
+++ ldes/shared/src/main/scala/run/cosy/ldes/LdesSpider.scala
@@ -25,7 +25,7 @@ class LdesSpider[F[_]: Concurrent, R <: RDF](
   
   
   /** given the ldes stream URL, crawl the nodes of that stream  */
-  def crawl(stream: RDF.URI[R]): F[fs2.Stream[F, RDF.Graph[R]]] =
+  def crawl(stream: RDF.URI[R]): F[fs2.Stream[F, fs2.Chunk[UriNGraph[R]]]] =
     import cats.syntax.all.toFunctorOps
     import cats.syntax.flatMap.toFlatMapOps
     for
@@ -35,15 +35,13 @@ class LdesSpider[F[_]: Concurrent, R <: RDF](
 
   /* This crawls pages forward and collects all tree.member observations...
    * This is very much tied to a particular use of ldes
-   * todo: it should be able to fetch the shEx to find out what to
-   *   collect, or an pattern object should be passed.
    * @returns an fs2.Stream of such observation mini graphs
    **/
   def crawlNodesForward(
       stream: RDF.URI[R],
       startNodes: Seq[PNGraph[R]],
       visitedRef: Ref[F, Set[RDF.URI[R]]]
-  ): fs2.Stream[F, RDF.Graph[R]] =
+  ): fs2.Stream[F, fs2.Chunk[UriNGraph[R]]] =
     import cats.syntax.all.toFlatMapOps
     fs2.Stream.unfoldLoopEval(startNodes) { nodes =>
       import cats.syntax.traverse.{*, given}
@@ -68,26 +66,12 @@ class LdesSpider[F[_]: Concurrent, R <: RDF](
           .rel(tree.node)
           .collect { case ung: UriNGraph[R] => ung }
         // we need to place the pointer on the Collection of each page
-        val collInPages: Seq[UriNGraph[R]] =
-          pages.map(ung => new UriNGraph[R](stream, ung.name, ung.graph))
-        val obs: RDF.Graph[R] = collInPages
-          .rel(tree.member)
-          .map(png =>
-            png.collect(
-              rdf.typ,
-              wgs84.location,
-              sosa.hasSimpleResult,
-              sosa.madeBySensor,
-              sosa.observedProperty,
-              sosa.resultTime
-            )()
-          )
-          .fold(Graph.empty)((g1, g2) => g1 union g2)
         (
-          obs,
+          fs2.Chunk(pages*),
           if nextPages.isEmpty then None
           else Some(nextPages)
         )
     }
   end crawlNodesForward
-  
+
+ 
--- scripts/jvm/src/main/scala/scripts/MiniCF.scala
+++ scripts/jvm/src/main/scala/scripts/MiniCF.scala
@@ -2,7 +2,8 @@ package scripts
 
 // This fetches data from a mini City Flows web server
 import cats.effect.unsafe.IORuntime
-import cats.effect.{unsafe, Concurrent, IO, Ref}
+import cats.effect.{Concurrent, IO, Ref, unsafe}
+import fs2.Chunk
 import org.http4s.EntityDecoder
 import org.http4s.client.Client
 import org.http4s.ember.client.*
@@ -26,22 +27,55 @@ object MiniCF:
 
   @main
   def crawlContainer(stream: String = "http://localhost:8080/ldes/miniCityFlows/stream#"): Unit =
-    val ioStr: IO[fs2.Stream[IO, RDF.Graph[JR]]] =
+    val streamUri: RDF.URI[JR] = ops.URI(stream)
+    
+    val ioStr: IO[fs2.Stream[IO, Chunk[UriNGraph[JR]]]] =
       AnHttpSigClient.emberAuthClient.flatMap { (client: Client[IO]) =>
         given web: Web[IO, JR] = new H4Web[IO, JR](client)
         val spider: LdesSpider[IO, JR] = new LdesSpider[IO, JR]
-        val streamUri: RDF.URI[JR] = ops.URI(stream)
         spider.crawl(streamUri)
       }
       
     val l: IO[List[RDF.Graph[JR]]] =
       fs2.Stream.eval(ioStr)
         .flatten
+        .unchunks
+        .map( uriNG => selectObs(uriNG, streamUri) )
+        .unchunks
         .reduce((g1, g2) => g1.union(g2))
         .compile[IO, IO, RDF.Graph[JR]]
         .toList
     l.unsafeRunSync().foreach(_.triples.foreach(println))
   end crawlContainer
+
+  import run.cosy.ldes.prefix as ldesPre
+  val foaf = prefix.FOAF[JR]
+  val tree = ldesPre.TREE[JR]
+  val sosa = ldesPre.SOSA[JR]
+  val wgs84 = ldesPre.WGS84[JR]
+  val ldes = ldesPre.LDES[JR]
+
+  /** select the observations in tree:Node of the ldes:Stream
+   *
+   * todo: it should be able to be given or fetch the shEx described in the
+   * ldes:Stream to decide what to select
+   * */
+  def selectObs(page: UriNGraph[JR], streamUrl: RDF.URI[JR]): fs2.Chunk[RDF.Graph[JR]] =
+    val collInPage = new UriNGraph[JR](streamUrl, page.name, page.graph)
+    val obs: Seq[RDF.Graph[JR]] = collInPage
+      .rel(tree.member)
+      .map(png =>
+        png.collect(
+          rdf.typ,
+          wgs84.location,
+          sosa.hasSimpleResult,
+          sosa.madeBySensor,
+          sosa.observedProperty,
+          sosa.resultTime
+        )()
+      )
+    fs2.Chunk(obs *)
+  //      .fold(Graph.empty)((g1, g2) => g1 union g2)
   
 
 end MiniCF
