2954d6893edab539e39ee675fa9f34719de5f616
Henry Story <henry.story@bblfish.net>
2023-05-19 20:29:33 +0200

    add Chris Davenport's license to his code

--- cache/shared/src/main/scala/io/chrisdavenport/mules/http4s/CachedResponse.scala
+++ cache/shared/src/main/scala/io/chrisdavenport/mules/http4s/CachedResponse.scala
@@ -1,6 +1,5 @@
 /*
- * Copyright 2021 bblfish.net
- *
+ * Copyright 2019 Chris Davenport
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
